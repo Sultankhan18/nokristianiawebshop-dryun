56d1d84623b9cc31480a6cdd83e77b26cf7d4c0e
Sultan <zohanwall@gmail.com>
2019-10-02 15:42:40 +0200

    Initial commit

---
+++ src/main/java/no/kristiania/webshop/ProductDao.java
@@ -0,0 +1,19 @@
+package no.kristiania.webshop;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ProductDao {
+
+    private List<String> products = new ArrayList<>();
+
+    public void insertProduct(String productName) {
+        products.add(productName);
+    }
+
+    public List<String> listAll() {
+        return products;
+    }
+}
+
